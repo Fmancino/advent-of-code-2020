ea1be546c2a79635e38fff7da487fb57d8f5511d
Fmancino <17271080+Fmancino@users.noreply.github.com>
2020-12-10 09:36:20 +0100

    day 10 work in progress

--- 10/main.py
+++ 10/main.py
@@ -1,29 +1,67 @@
 #!/usr/bin/env python3
 import sys
+from itertools import permutations
 
 def parse_in(std_in):
-    return [int(i) for i in std_in]
-
-def first_task(parsed):
+    parsed = [int(i) for i in std_in]
     parsed.sort()
     parsed.insert(0, 0)
+    parsed.append(max(parsed) + 3)
+    return parsed
+
+def first_task(parsed):
     diff_3 = 0
     diff_1 = 0
-    for idx, val in enumerate(parsed):
-        if idx == len(parsed) - 1:
-            to_next = 3
-        else:
-            to_next = parsed[idx + 1] - val
+    for idx, val in enumerate(parsed[0:-1]):
+        to_next = parsed[idx + 1] - val
         if to_next == 3:
             diff_3 += 1
         elif to_next == 1:
             diff_1 += 1
         elif to_next != 2:
             raise ValueError('bad input')
+    print(len(parsed))
+    print(diff_3)
     return diff_1 * diff_3
 
-def second_task(parsed, first_res):
-    return 0
+def is_valid(perm):
+    for idx, val in enumerate(perm[0:-1]):
+        to_next = abs(perm[idx + 1] - val)
+        if to_next not in [1,2,3]:
+            return False
+    print(perm)
+    return True
+
+
+def second_task(parsed):
+    smaller = []
+    last_idx = 0
+    print(parsed)
+    for idx, val in enumerate(parsed[0:-1]):
+        print(smaller)
+        to_next = parsed[idx + 1] - val
+        if to_next == 3:
+            smaller.append(parsed[last_idx:idx+1])
+            last_idx = idx + 1
+    valid_perms = []
+    for s in smaller:
+        valid_perm = 0
+        if len(s) in [1,2,3]:
+            # one is not interesting
+            continue
+        for p in permutations(s[1:-1]):
+            p = list(p)
+            p.append(s[-1])
+            p.insert( 0, s[0])
+            print(p)
+            if is_valid(p):
+                valid_perm += 1
+        valid_perms.append(valid_perm)
+    print(valid_perms)
+    mult = 1
+    for p in valid_perms:
+        mult *= p
+    return mult
 
 def main():
     std_in = sys.stdin.readlines()
@@ -33,7 +71,7 @@ def main():
     res = first_task(parsed)
     print(f"task 1: {res}")
 
-    res = second_task(parsed, res)
+    res = second_task(parsed)
     print(f"task 2: {res}")
 
 if __name__ == "__main__":
