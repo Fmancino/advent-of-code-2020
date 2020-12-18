3dd96cf507638c56209a0a9849278504132274a3
Fmancino <17271080+Fmancino@users.noreply.github.com>
2020-12-18 23:41:59 +0100

    star 2 day 18

--- 18/main.py
+++ 18/main.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
 import sys
-import itertools
-import copy
 
 def parse_in(std_in):
     return [s.strip() for s in std_in]
-
 
 def plus(a, b):
     return a + b
@@ -15,18 +12,37 @@ def times(a, b):
 
 OPER_DICT = {'+': plus, '*': times}
 
+def add_parens(text):
+    _open = 1
+    for idx, l in enumerate(text):
+        if l == '(':
+            _open += 1
+        elif l == ')':
+            _open -= 1
+            if _open == 0:
+                return '(' + text[:idx] + ')' + text[idx:]
+    return '(' + text + ')'
+
 def calculate(text):
     while True:
         if text[0] == '(':
             text = calculate(text[1:])
         space = text.find(' ')
-        if space > 0:
-            first_num = int(text[:space])
-            text = text[space+1:]
-        else:
-            raise ValueError
+        for idx, l in enumerate(text):
+            if l == ' ':
+                space = idx
+                break
+            elif l == ')':
+                return text[:idx] + text[idx+1:]
+            elif idx == len(text) - 1:
+                return text
+        first_num = int(text[:space])
+        text = text[space+1:]
         operation = OPER_DICT[text[0]]
         text = text[2:]
+        #Comment next 2 lines to get star 1
+        if operation == times:
+            text = add_parens(text)
         if text[0] == '(':
             text = calculate(text[1:])
         for idx, l in enumerate(text):
@@ -47,22 +63,19 @@ def calculate(text):
         res = operation(first_num, next_num)
         text = str(res) + text
 
-def first_task(parsed_lines):
+def second_task(parsed_lines):
     _sum = 0
     for line in parsed_lines:
         _sum += int(calculate(line))
     return _sum
-
-def second_task(parsed_lines):
-    return 0
 
 def main():
     std_in = sys.stdin.readlines()
 
     parsed = parse_in(std_in)
 
-    res = first_task(parsed)
-    print(f"task 1: {res}")
+    #res = first_task(parsed)
+    #print(f"task 1: {res}")
 
     res = second_task(parsed)
     print(f"task 2: {res}")
